787a4b999e3368e2c59b86258b03aa5d4b7b5459
gsanchz <125102426+gsanchz@users.noreply.github.com>
2024-01-01 16:50:57 +0100

    Update 240101_Array_Pair_Sum_Divisible.py

--- 240101_Array_Pair_Sum_Divisible.py
+++ 240101_Array_Pair_Sum_Divisible.py
@@ -1,11 +1,70 @@
-# With numpy (no valid solution)
-def canPair(self, nuns, k):
-		if len(nuns)%2 == 1:
-		    return False
-        nuns_div = np.array([nun%k for nun in nuns])
-        if (nuns_div == 0).sum() %2 == 1:
+"""
+Given an array of integers nums and a number k, write a function that returns true
+if given array can be divided into pairs such that sum of every pair is divisible by k.
+#######################################################################################
+Example 1 :
+
+Input : 
+nums = [9, 5, 7, 3]
+k = 6
+Output: 
+True
+Explanation: 
+{(9, 3), (5, 7)} is a 
+possible solution. 9 + 3 = 12 is divisible
+by 6 and 7 + 5 = 12 is also divisible by 6.
+########################################################################################
+Example 2:
+
+Input : 
+nums = [4, 4, 4]
+k = 4
+Output: 
+False
+Explanation: 
+You can make 1 pair at max, leaving a single 4 unpaired.
+#######################################################################################
+
+Your Task:
+You don't need to read or print anything. Your task is to complete the function canPair()
+which takes array nums and k as input parameter and returns true if array can be divided
+into pairs such that sum of every pair is divisible by k otherwise returns false.
+
+Expected Time Complexity: O(n)
+Expected Space Complexity : O(n)
+
+Constraints:
+1 <= length( nums ) <= 105
+1 <= numsi <= 105
+1 <= k <= 105
+"""
+
+# My second solution (0.22)
+
+    def canPair(self, nuns, k):
+        if len(nuns)%2 == 1:
             return False
+	rem_dict = dict(zip(range(k),[0]*k))
+	for nun in nuns:
+	    rem_dict[nun%k] += 1
+	if rem_dict[0]%2 == 1:
+	    return False
+	for j in range(1,k//2):
+	    if rem_dict[j] != rem_dict[k-j]:
+	        return False
+	return True
+
+# With numpy (my first no valid solution)
+import numpy as np
+
+class Solution:
+    def canPair(self, nuns, k):
+        if len(nuns)%2 == 1:
+	    return False
+	nuns_div = np.array([nun%k for nun in nuns])
+	if (nuns_div == 0).sum() %2 == 1:
+	    return False
         for j in range(1, k//2 + 1):
-            if (nuns_div == j).sum() != (nuns_div == k-j).sum()
-                return False
+	    if (nuns_div == j).sum() != (nuns_div == k-j).sum()
+		return False
         return True
