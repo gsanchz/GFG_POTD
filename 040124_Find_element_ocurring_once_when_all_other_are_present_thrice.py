8bd843b983ca5e6a1f7651caf641c71fb558be14
gsanchz <125102426+gsanchz@users.noreply.github.com>
2024-01-03 20:31:55 +0100

    Create 040124_Find_element_ocurring_once_when_all_other_are_present_thrice.py

---
+++ 040124_Find_element_ocurring_once_when_all_other_are_present_thrice.py
@@ -0,0 +1,76 @@
+"""
+Given an array of integers arr[] of length N, every element appears thrice
+except for one which occurs once. Find that element which occurs once.
+
+############################################################################
+Example 1:
+
+Input:
+N = 4
+arr[] = {1, 10, 1, 1}
+Output:
+10
+Explanation:
+10 occurs once in the array while the other
+element 1 occurs thrice.
+############################################################################
+Example 2:
+
+Input:
+N = 10
+arr[] = {3, 2, 1, 34, 34, 1, 2, 34, 2, 1}
+Output:
+3
+Explanation:
+All elements except 3 occurs thrice in the array.
+#############################################################################
+
+Your Task:
+You do not need to take any input or print anything. You task is to complete
+the function singleElement() which takes an array of integers arr and an integer
+N which finds and returns the element occuring once in the array.
+
+Constraints:
+1 ≤ N ≤ 105
+-109 ≤ A[i] ≤ 109
+
+Expected Time Complexity: O(N).
+Expected Auxiliary Space: O(1).
+"""
+
+#Second right solution (0.33)
+class Solution:
+    def singleElement(self,arr,N):
+        arr = sorted(arr)
+        window_width = 3
+        for i in range(0,N,3):
+            start_pos = i
+            final_pos = start_pos + window_width
+            subarray = arr[start_pos:final_pos]
+            max_in_subarray = max(subarray)
+            min_in_subarray = min(subarray)
+            if max_in_subarray != min_in_subarray:
+                try:
+                    subarray.remove(max_in_subarray)
+                    subarray.remove(max_in_subarray)
+                    return min_in_subarray
+                except:
+                    return max_in_subarray
+            else:
+                pass
+        return arr[N-1]
+      
+#First wrong solution (takes too much time to run)
+class Solution:
+    def singleElement(self,arr, N):
+        def take_off(ele,arr):
+            arr.remove(ele)
+            arr.remove(ele)
+            arr.remove(ele)
+            return arr
+        while len(arr) > 0:
+            try:
+                ele = arr[0]
+                arr = take_off(ele,arr)
+            except:
+                return ele
